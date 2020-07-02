52b137d2967c52923a046c5af260cf0baff3f5cf
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-07-02 19:36:20 +0300

    nucleo_f429zi_audio.c

--- platform/adar7251/drivers/audio/nucleo_f429zi_audio.c
+++ platform/adar7251/drivers/audio/nucleo_f429zi_audio.c
@@ -149,14 +149,12 @@ void sai_prepare(struct sai_device *sai_dev, int channels) {
 }
 
 void sai_start(struct sai_device *sai_dev, int channels) {
-
+	sai_dev->sai_active = 0;
 	sai_dev->buf_num = 0;
 
 	HAL_SAI_Receive_DMA(sai_device.sai_hw_dev, (uint8_t *)&sai_device.sai_buf[0], sizeof(sai_device.sai_buf) / 4);
 
 //	sai_dev->sai_cur_buf = NULL;
-//	sai_dev->sai_active = 1;
-	sai_dev->buf_num = 0;
 }
 
 void sai_stop(struct sai_device *sai_dev) {
